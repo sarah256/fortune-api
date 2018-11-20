2d21dcbaa2e61745160fc5dfd3506a741544959c
srieger <srieger256@gmail.com>
2018-11-20 13:33:01 -0500

    Add ability to randomly select a datfile

--- main.go
+++ main.go
@@ -1,16 +1,63 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
-    "github.com/gorilla/mux"
+    // "github.com/gorilla/mux"
+    // "log"
+    // "net/http"
+	"math/rand"
+	"time"
+	"fmt"
+    "path/filepath"
+    "os"
+    "strings"
+    "bufio"
 )
 
 func main() {
-    router := mux.NewRouter()
-    router.HandleFunc("/datfiles", GetFortune).Methods("GET")
-    log.Fatal(http.ListenAndServe(":8000", router))
-}
+    var files []string
+    var datfiles []string
 
-func GetFortune(w http.ResponseWriter, r *http.Request) {}
+    root := "datfiles"
+    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+        files = append(files, path)
+        return nil
+    })
+    if err != nil {
+        panic(err)
+    }
+    for _, file := range files {
+    	if !strings.Contains(file, "CMakeLists.txt") && !strings.Contains(file, "off") && !strings.Contains(file, "README.md") {
+        	datfiles = append(datfiles, file)
+    	}
+    }
+
+	rand.Seed(time.Now().UnixNano())
+	a := rand.Intn(len(datfiles))
+	fmt.Println(a)
+	fmt.Println(len(datfiles))
+
+	// Select random file
+	randFile := datfiles[a]
+
+	file, err := os.Open(randFile)
+
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    defer file.Close()
+
+    reader := bufio.NewReader(file)
+    scanner := bufio.NewScanner(reader)
+
+    for scanner.Scan() {
+        // fmt.Println(scanner.Text())
+        // TODO: process each quote here
+    }
+
+    // router := mux.NewRouter()
+    // router.HandleFunc("/people", GetPeople).Methods("GET")
+    // router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+    // log.Fatal(http.ListenAndServe(":8000", router))
+}
