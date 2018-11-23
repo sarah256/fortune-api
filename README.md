3019fff9d91352ccec056b497e909c0be8ca40ce
srieger <srieger256@gmail.com>
2018-11-23 03:44:45 -0500

    Add ability to choose specific fortune type

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# fortune-api :crystal_ball:
+# :crystal_ball: fortune-api :crystal_ball:
 
 ## Description
 A RESTful API for the classic fortune command-line utility (in the fortune-mod package)
+*Note: This API is still under construction*
 
 ## Adding Fortunes
 To add a new fortune, go to the `datfiles` directory, and choose the correspoding file to contribute to.  If none of the files fit, feel free to make your own!  As with the original, fortunes deemed to be not-so-wholesome should be confined to the `datfiles/off` directory.
--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func getFortune(filename string, fortunes int) string {
 	// filename : the file the fortune is being selected from
 	// fortunes : the total number of fortunes in the file
 	// Returns : the fortune in the form of a formatted string
+
 	file, err := os.Open(filename)
 
     if err != nil {
@@ -30,10 +31,10 @@ func getFortune(filename string, fortunes int) string {
     scanner := bufio.NewScanner(reader)
 
 	rand.Seed(time.Now().UnixNano())
-	b := rand.Intn(fortunes)
+	b := rand.Intn(fortunes-1)
 	fmt.Println("\nChose fortune", b, "of", fortunes, "total fortunes")
 
-    count := 1
+    count := 0
     fortune := ""
 
     for scanner.Scan() {
@@ -56,6 +57,7 @@ func getRandomFortune() string {
     var files []string
     var datfiles []string
 
+    // Get list of all fortune files in datfiles
     root := "datfiles"
     err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
         files = append(files, path)
@@ -70,6 +72,7 @@ func getRandomFortune() string {
     	}
     }
 
+    // Choose a random datfile
 	rand.Seed(time.Now().UnixNano())
 	a := rand.Intn(len(datfiles))
 	fmt.Println("\nChose datfile", a, "of", len(datfiles), "total datfiles")
@@ -102,8 +105,33 @@ func getRandomFortune() string {
 	return getFortune(randFile, fortunes)
 }
 
-func getSpecificFortune() {
+func getSpecificFortuneType(fortuneType string) string {
+	// Get a specific genre of fortune
+	// fortuneType : the genre of fortune; must be a file within datfiles
 
+	filePath := fmt.Sprintf("%s%s", "datfiles\\", fortuneType)
+
+	file, err := os.Open(filePath)
+
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    defer file.Close()
+
+    reader := bufio.NewReader(file)
+    scanner := bufio.NewScanner(reader)
+
+    fortunes := 0
+
+    for scanner.Scan() {
+    	if scanner.Text() == "%" {
+    		fortunes++
+    	}
+    }
+
+	return getFortune(filePath, fortunes)
 }
 
 func main() {
@@ -111,6 +139,10 @@ func main() {
     fortune := getRandomFortune()
 
     fmt.Println("\n", fortune)
+
+    fortune2 := getSpecificFortuneType("goedel")
+
+    fmt.Println("\n", fortune2)
 
     // router := mux.NewRouter()
     // router.HandleFunc("/people", GetPeople).Methods("GET")
